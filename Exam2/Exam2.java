c78fcadecfcdcc0033c8d9feab058c20448fb204
angwoo <ssaangowo@gmail.com>
2023-09-12 17:40:48 +0900

    시험

---
+++ Exam2/Exam2.java
@@ -0,0 +1,70 @@
+package Exam2;
+
+import java.util.Scanner;
+
+public class Exam2 {
+    public static void main(String[] args) {
+
+        // 높이를 입력해주세요 : 3                // 출력
+        for(int i = 0; i < 3; i++) {
+            System.out.println(" ");
+            for (int j = 1; j < 5; j += 2){
+
+                System.out.println("*");
+
+            }
+
+        }
+        /*
+
+         *
+
+        ***
+
+       *****
+
+         */
+
+        // 높이를 입력해주세요 : 5
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         */
+
+        // 높이를 입력해주세요 : 7
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         ***********
+
+         *************
+
+         */
+
+    }
+
+}
